5870fd99b98cd29cdd48f541848ccb0d997bdf51
jivan-deep <jivan@websultanate.com>
2023-06-26 13:39:55 +0530

    make changes on decryption

---
+++ middlewares/decrypt.mjs
@@ -0,0 +1,21 @@
+import crypto from 'crypto';
+
+export const decryptPass = (encryptedPassword) => {
+	try {
+    console.log("encryptedPassword",encryptedPassword);
+		const algorithm = 'aes-192-cbc';
+		const password = process.env.CRYPTO_KEY;
+        console.log("password",password);
+		const key = crypto.scryptSync(password, 'salt', 24);
+		const iv = Buffer.alloc(16, 0);
+		const decipher = crypto.createDecipheriv(algorithm, key, iv);
+		const encrypted = encryptedPassword;
+        console.log(encrypted);
+		let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+		decrypted += decipher.final('utf8');
+		return decrypted;
+	} catch (e) {
+        console.log("eee",e);
+		return null;
+	}
+};
