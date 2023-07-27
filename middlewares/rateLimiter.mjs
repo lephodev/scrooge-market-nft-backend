ccde4ef9563b1f14e7759bce0eec9f7a6cdd53fb
jivan-deep <jivan@websultanate.com>
2023-07-27 13:07:28 +0530

    added rateLmiter in 24 hours

--- middlewares/rateLimiter.mjs
+++ middlewares/rateLimiter.mjs
@@ -3,19 +3,9 @@ import { RateLimiterMemory } from "rate-limiter-flexible";
 import { getClientIp } from "request-ip";
 import { getIpAdress } from "./IpAddress.mjs";
 
-// const authLimiter = rateLimit({
-//   windowMs: 5 * 60 * 1000, // 24 hours in milliseconds
-//   max: 4,
-//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//   legacyHeaders: false,
-//   message: { message: "To many requests found" },
-//   statusCode: 429,
-// });
-
-const MAX_REQUEST_LIMIT = 2;
-const MAX_REQUEST_WINDOW = 5 * 60; // Per 5 minutes by IP
-const TOO_MANY_REQUESTS_MESSAGE = "Too many requests";
-
+const MAX_REQUEST_LIMIT = 4;
+const MAX_REQUEST_WINDOW = 24 * 60 * 60; // Per 24 hours by IP
+const TOO_MANY_REQUESTS_MESSAGE = "Too many requests found";
 const options = {
   duration: MAX_REQUEST_WINDOW,
   points: MAX_REQUEST_LIMIT,
@@ -31,7 +21,7 @@ const authLimiter = (req, res, next) => {
       next();
     })
     .catch(() => {
-      res.status(429).json({ message: "TOO_MANY_REQUESTS_MESSAGE" });
+      res.status(429).json({ message: TOO_MANY_REQUESTS_MESSAGE });
     });
 };
 
