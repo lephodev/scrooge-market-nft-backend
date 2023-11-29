09167d49f21ff65249d66d8aff091fe01dd6455f
jivan-deep <jivan@websultanate.com>
2023-11-29 16:36:39 +0530

    update webhook

--- index.mjs
+++ index.mjs
@@ -518,7 +518,10 @@ app.post("/api/bitcartcc-notification", async (req, res) => {
 });
 
 app.post("/api/approvely-webhook", async (req, res) => {
-  console.log("webhook called");
+  console.log("webhook called", req);
+  const rawPayload = JSON.stringify(req.body);
+  console.log("rawPayload", rawPayload);
+
   res.send({ success: true });
 });
 
