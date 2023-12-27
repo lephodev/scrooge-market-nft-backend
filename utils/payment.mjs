e779c678094c84fdd7d62fd064b5e6282ee0f00b
jivan-deep <jivan@websultanate.com>
2023-12-27 14:26:11 +0530

    added bonus check

--- utils/payment.mjs
+++ utils/payment.mjs
@@ -301,62 +301,69 @@ export function getAnAcceptPaymentPage(body, user, callback) {
 
 // call this function when webhook trigger to fetch transaction details and extract the email to find user with ewmail. and update user wallet iwt thw wmail
 export const getTransactionDetails = (body, callback) => {
-  let details = JSON.parse(body);
-  var merchantAuthenticationType =
-    new ApiContracts.MerchantAuthenticationType();
-  merchantAuthenticationType.setName(process.env.AUTHORIZE_LOGIN_ID);
+  try {
+    let details = JSON.parse(body);
+    var merchantAuthenticationType =
+      new ApiContracts.MerchantAuthenticationType();
+    merchantAuthenticationType.setName(process.env.AUTHORIZE_LOGIN_ID);
 
-  merchantAuthenticationType.setTransactionKey(
-    process.env.AUTHORIZE_TRANSACTION_KEY
-  );
+    merchantAuthenticationType.setTransactionKey(
+      process.env.AUTHORIZE_TRANSACTION_KEY
+    );
 
-  var getRequest = new ApiContracts.GetTransactionDetailsRequest();
-  getRequest.setMerchantAuthentication(merchantAuthenticationType);
-  getRequest.setTransId(JSON.parse(details?.payload?.id));
+    var getRequest = new ApiContracts.GetTransactionDetailsRequest();
+    getRequest.setMerchantAuthentication(merchantAuthenticationType);
+    getRequest.setTransId(JSON.parse(details?.payload?.id));
 
-  console.log(JSON.stringify(getRequest.getJSON(), null, 2));
+    console.log(JSON.stringify(getRequest.getJSON(), null, 2));
 
-  var ctrl = new ApiControllers.GetTransactionDetailsController(
-    getRequest.getJSON()
-  );
+    var ctrl = new ApiControllers.GetTransactionDetailsController(
+      getRequest.getJSON()
+    );
 
-  ctrl.setEnvironment("https://api.authorize.net/xml/v1/request.api");
+    ctrl.setEnvironment("https://api.authorize.net/xml/v1/request.api");
 
-  ctrl.execute(function () {
-    var apiResponse = ctrl.getResponse();
+    ctrl.execute(function () {
+      var apiResponse = ctrl.getResponse();
 
-    var response = new ApiContracts.GetTransactionDetailsResponse(apiResponse);
+      var response = new ApiContracts.GetTransactionDetailsResponse(
+        apiResponse
+      );
 
-    if (response != null) {
-      if (
-        response.getMessages().getResultCode() ==
-        ApiContracts.MessageTypeEnum.OK
-      ) {
-        console.log(
-          "Transaction Id : " + response.getTransaction().getTransId()
-        );
-        console.log(
-          "Transaction Type : " + response.getTransaction().getTransactionType()
-        );
-        console.log(
-          "Message Code : " + response.getMessages().getMessage()[0].getCode()
-        );
-        console.log(
-          "Message Text : " + response.getMessages().getMessage()[0].getText()
-        );
+      if (response != null) {
+        if (
+          response.getMessages().getResultCode() ==
+          ApiContracts.MessageTypeEnum.OK
+        ) {
+          console.log(
+            "Transaction Id : " + response.getTransaction().getTransId()
+          );
+          console.log(
+            "Transaction Type : " +
+              response.getTransaction().getTransactionType()
+          );
+          console.log(
+            "Message Code : " + response.getMessages().getMessage()[0].getCode()
+          );
+          console.log(
+            "Message Text : " + response.getMessages().getMessage()[0].getText()
+          );
+        } else {
+          console.log("Result Code: " + response.getMessages().getResultCode());
+          console.log(
+            "Error Code: " + response.getMessages().getMessage()[0].getCode()
+          );
+          console.log(
+            "Error message: " + response.getMessages().getMessage()[0].getText()
+          );
+        }
       } else {
-        console.log("Result Code: " + response.getMessages().getResultCode());
-        console.log(
-          "Error Code: " + response.getMessages().getMessage()[0].getCode()
-        );
-        console.log(
-          "Error message: " + response.getMessages().getMessage()[0].getText()
-        );
+        console.log("Null Response.");
       }
-    } else {
-      console.log("Null Response.");
-    }
 
-    callback(response);
-  });
+      callback(response);
+    });
+  } catch (error) {
+    console.log("errorerror", error);
+  }
 };
