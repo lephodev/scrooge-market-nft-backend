700685d8887ba2dfec62c92fc74f5ac4b0e2a93f
jivan-deep <jivan@websultanate.com>
2024-01-25 16:19:27 +0530

    added data on crypto to gc

--- config/rewards.mjs
+++ config/rewards.mjs
@@ -1438,6 +1438,9 @@ const getDecodedData = async (recipt) => {
         if (cryptoUsd === 11.5884) {
           return 9.99;
         }
+        if (cryptoUsd === 23.1884) {
+          return 19.99;
+        }
         return parseInt(Math.round(cryptoUsd));
       }
 
@@ -1465,6 +1468,9 @@ const getDecodedData = async (recipt) => {
       if (cryptoUsd === 11.5884) {
         return 9.99;
       }
+      if (cryptoUsd === 23.1884) {
+        return 19.99;
+      }
       return pids[Math.round(cryptoUsd)];
     }
     return cryptoAmt;
@@ -1475,7 +1481,7 @@ const getDecodedData = async (recipt) => {
 
 export async function convertCryptoToGoldCoin(req, res) {
   const { address, transactionHash } = req.params;
-  const { promoCode } = req.query;
+  const { promoCode, usd } = req.query;
   const {
     user: {
       _id: userId,
@@ -1529,12 +1535,12 @@ export async function convertCryptoToGoldCoin(req, res) {
       });
     }
     console.log("recipt", recipt);
-    const amt = await getDecodedData(recipt);
+    const amt = usd;
     console.log("amt", amt);
-    let cealAmount = Math.ceil(amt);
-    console.log("cealAmount", cealAmount);
+    // let cealAmount = Math.ceil(amt);
+    console.log("cealAmount", usd);
     const data = await db.get_marketplace_gcPackagesDB().findOne({
-      priceInBUSD: cealAmount.toString(),
+      priceInBUSD: amt.toString(),
     });
     console.log("datadata--------", data);
     if (!data)
@@ -1734,7 +1740,7 @@ export async function convertCryptoToGoldCoin(req, res) {
       .get_scrooge_usersDB()
       .findOneAndUpdate(
         { _id: ObjectId(userId) },
-        { $set: { isGCPurchase: true } }
+        { $push: { megaOffer: parseFloat(amt) } }
       );
     let getUserDetail = await db
       .get_scrooge_usersDB()
