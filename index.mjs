74b513929fa147107325b818339859b3db9bce97
jivan-deep <jivan@websultanate.com>
2024-01-29 17:22:12 +0530

    Fixed  invoice template

--- index.mjs
+++ index.mjs
@@ -671,7 +671,8 @@ app.post("/api/authorize-webhook", async (req, res) => {
                         (parseFloat(findPromoData?.discountInPercent) / 100)
                     : findPromoData?.coupanType === "2X"
                     ? parseInt(data.freeTokenAmount)
-                    : 0, amount //amount?.toString() === "9.99"
+                    : 0,
+                  amount //amount?.toString() === "9.99"
                   // ? 1500
                   // : 0
                 );
@@ -1023,21 +1024,5 @@ app.get(
 app.listen(PORT, () => {
   console.log("Server is running.", PORT);
 });
-
-// const reciptPayload = {
-//   username: "jivan",
-//   email: "jivanwebsul@gmail.com",
-//   invoicDate: moment(new Date()).format("D MMMM  YYYY"),
-//   paymentMethod: "Credit Card Purchase",
-//   packageName: "Gold Coin Purchase",
-//   goldCoinQuantity: 30000000,
-//   tokenQuantity: 3000,
-//   purcahsePrice: "9.99",
-//   Tax: 0,
-//   firstName: "jivan",
-//   lastName: "Tiwari",
-// };
-
-// await InvoiceEmail("jivanwebsul@gmail.com", reciptPayload);
 
 export default app;
