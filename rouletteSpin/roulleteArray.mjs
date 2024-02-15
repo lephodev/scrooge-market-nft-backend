8e58a412732eb6322e552728dd058e8c570f4a65
jivan-deep <jivan@websultanate.com>
2024-02-15 10:26:37 +0530

    updated loyality wheel

--- rouletteSpin/roulleteArray.mjs
+++ rouletteSpin/roulleteArray.mjs
@@ -38,14 +38,14 @@ export const BigWheelPlaces = [
 ];
 
 export const LoyaltyWheelPlaces = [
-  { token: 150, chances: 10 },
-  { token: 175, chances: 10 },
-  { token: 200, chances: 10 },
-  { token: 225, chances: 10 },
-  { token: 250, chances: 10 },
-  { token: 150, chances: 10 },
-  { token: 175, chances: 10 },
-  { token: 200, chances: 10 },
-  { token: 225, chances: 10 },
-  { token: 250, chances: 10 },
+  { token: 500, chances: 0.5 },
+  { token: 275, chances: 12.375 },
+  { token: 200, chances: 12.375 },
+  { token: 225, chances: 12.375 },
+  { token: 250, chances: 12.375 },
+  { token: 1000, chances: 0.5 },
+  { token: 275, chances: 12.375 },
+  { token: 200, chances: 12.375 },
+  { token: 225, chances: 12.375 },
+  { token: 250, chances: 12.375 },
 ];
