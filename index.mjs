5d9496ede210cefb16f256b02c65bf34d477e782
jivan-deep <jivan@websultanate.com>
2024-02-26 11:06:38 +0530

    added est time on purchase limit

--- index.mjs
+++ index.mjs
@@ -965,8 +965,6 @@ app.get(
     const startOfDay = new Date();
     console.log("startOfDay-------", startOfDay);
     startOfDay.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0 for the start of the day
-
-    console.log("estDate", estDate);
     startOfDay.setHours(5, 0, 0, 0);
     console.log("startOfDay limit ", startOfDay);
     const query = {
