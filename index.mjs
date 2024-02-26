681ea5618dc1dea6fee4d2cccf6699a384d9259e
jivan-deep <jivan@websultanate.com>
2024-02-26 11:05:23 +0530

    added est time on purchase limit

--- index.mjs
+++ index.mjs
@@ -966,7 +966,9 @@ app.get(
     console.log("startOfDay-------", startOfDay);
     startOfDay.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0 for the start of the day
 
-    console.log("startOfDay", startOfDay);
+    console.log("estDate", estDate);
+    startOfDay.setHours(5, 0, 0, 0);
+    console.log("startOfDay limit ", startOfDay);
     const query = {
       transactionType: "CC To Gold Coin",
       "userId._id": userId,
