af0d153bd52db613fcccc0de1ac129f19505e33f
jivan-deep <jivan@websultanate.com>
2024-03-08 11:38:36 +0530

    added data

--- index.mjs
+++ index.mjs
@@ -964,7 +964,7 @@ app.get(
     console.log("getGCPurcahseLimitPerDay", userId);
     const startOfDay = new Date();
     console.log("startOfDay-------", startOfDay);
-    // startOfDay.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0 for the start of the day
+    startOfDay.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0 for the start of the day
     // startOfDay.setHours(5, 0, 0, 0);
     console.log("startOfDay limit ", startOfDay);
     const query = {
