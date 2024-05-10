231dec97392f71789738de6425f51cbf06d20322
jivan-deep <jivan@websultanate.com>
2024-05-10 16:42:40 +0530

    added data

--- middlewares/decrypt.mjs
+++ middlewares/decrypt.mjs
@@ -1,18 +1,18 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
 export const decryptPass = (encryptedPassword) => {
-	try {
-		const algorithm = 'aes-192-cbc';
-		const password = "gvytbffvsca#a%#$%#$j^$m#NHM4A645335";
-		const key = crypto.scryptSync(password, 'salt', 24);
-		const iv = Buffer.alloc(16, 0);
-		const decipher = crypto.createDecipheriv(algorithm, key, iv);
-		const encrypted = encryptedPassword;
-		let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-		decrypted += decipher.final('utf8');
-		return decrypted;
-	} catch (e) {
-        console.log("error in decryption",e);
-		return null;
-	}
+  try {
+    const algorithm = "aes-192-cbc";
+    const password = "gvytbffvsca#a%#$%#$j^$m#NHM4A645335";
+    const key = crypto.scryptSync(password, "salt", 24);
+    const iv = Buffer.alloc(16, 0);
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    const encrypted = encryptedPassword;
+    let decrypted = decipher.update(encrypted, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
+  } catch (e) {
+    console.log("error in decryption", e);
+    return null;
+  }
 };
