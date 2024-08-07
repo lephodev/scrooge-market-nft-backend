757d3b68eedf61a57207967524c397f6b5f8d05b
jivan-deep <jivan@websultanate.com>
2024-08-07 18:33:18 +0530

    fixed the error issue

--- index.mjs
+++ index.mjs
@@ -1111,7 +1111,7 @@ app.get(
     startOfDay.setDate(
       user?.megaOffer?.length >= 3
         ? startOfDay.getDate() - 3
-        : user?.megaOffer.includes(99.99)
+        : user?.megaOffer?.includes(99.99)
         ? startOfDay.getDate() - 7
         : startOfDay.getDate() - 1
     );
