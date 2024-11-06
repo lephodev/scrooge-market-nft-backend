96d012960c6ee374b21f900d51ce5cc50e9ce3fb
Adarsh <adarsh@websultanate.com>
2024-11-06 15:16:27 +0530

    udated items in payment session

--- services/checkout.services.mjs
+++ services/checkout.services.mjs
@@ -82,19 +82,11 @@ export const getPaymentSession = async (body, req) => {
       // },
       enabled_payment_methods: ["card", "applepay", "googlepay"],
       // "disabled_payment_methods": ["eps", "ideal", "knet"],
-      // "items": [{
-      //   "reference": "$10 GC",
-      //   "commodity_code": "1234",
-      //   "unit_of_measure": "each",
-      //   "total_amount": 10,
-      //   "tax_amount": 0,
-      //   "discount_amount": 0,
-      //   "url": "string",
-      //   "image_url": "string",
-      //   "name": "Gold Necklace",
-      //   "quantity": 1,
-      //   "unit_price": 10
-      //   }],
+      "items": [{
+        "name": "Promo",
+        "quantity": 1,
+        "unit_price": 10
+        }],
       risk: {
         enabled: false,
       },
