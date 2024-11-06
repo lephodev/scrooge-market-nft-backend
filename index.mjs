2e5305a50ab5f1a7506a1d3803c75731200fba6e
Adarsh <adarsh@websultanate.com>
2024-11-06 15:52:52 +0530

    removed payment declined webhook

--- index.mjs
+++ index.mjs
@@ -1928,8 +1928,8 @@ app.post("/api/checkout-payments-webhook", async (req, res) => {
   try {
     console.log("req.body in checkout webhoook", req.body.data.source.billing_address, req.body);
     if (
-      req.body.type === "payment_approved" ||
-      req.body.type === "payment_declined"
+      req.body.type === "payment_approved" 
+      // || req.body.type === "payment_declined"
     ) {
       await checkoutWebHook(req.body);
     }
