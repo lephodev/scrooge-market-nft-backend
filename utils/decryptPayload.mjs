dff675f08180d5f3989742e616facb9ced930f0c
Adarsh <adarsh@websultanate.com>
2024-12-12 13:50:35 +0530

    updated log to check issue

--- utils/decryptPayload.mjs
+++ utils/decryptPayload.mjs
@@ -3,7 +3,7 @@ import CryptoJS from "crypto-js";
 const secretKey = process.env.TOKEN_ENCRYPTION_STRING;
 
 const decryptPayload = (req, res, next)=>{
-    console.log("decryptted data",req.body);
+    console.log("decryptted data",req.originalUrl, req.body);
     if(Object.keys(req?.body).length ){
         const bytes = CryptoJS.AES.decrypt(req?.body?.payload, secretKey);
         const decryptedPayload = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
