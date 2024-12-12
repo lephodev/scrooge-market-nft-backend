3d1c316161930d8fa3f90e3f290b125ba45b9b40
Adarsh <adarsh@websultanate.com>
2024-12-12 16:22:03 +0530

    added console to avoid unneccesary error

--- utils/decryptPayload.mjs
+++ utils/decryptPayload.mjs
@@ -3,14 +3,19 @@ import CryptoJS from "crypto-js";
 const secretKey = process.env.TOKEN_ENCRYPTION_STRING;
 
 const decryptPayload = (req, res, next)=>{
-    console.log("decryptted data",req.originalUrl, req.body);
-    if(Object.keys(req?.body).length ){
-        const bytes = CryptoJS.AES.decrypt(req?.body?.payload, secretKey);
-        const decryptedPayload = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        req.body = decryptedPayload;
-        console.log("Decrypted Payload:", decryptedPayload, bytes.toString(CryptoJS.enc.Utf8));
+    try{
+        console.log("decryptted data",req.originalUrl, req.body);
+        if(Object.keys(req?.body).length ){
+            const bytes = CryptoJS.AES.decrypt(req?.body?.payload, secretKey);
+            const decryptedPayload = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+            req.body = decryptedPayload;
+            console.log("Decrypted Payload:", decryptedPayload, bytes.toString(CryptoJS.enc.Utf8));
+        }
+        next();
+    }catch(error){
+        console.log("error in payload decryption", error);
     }
-    next();
+    
 }
 
 export default decryptPayload;
